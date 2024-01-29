fd749db2cca2b52f50a4e439fe024f736b27de7c
Kacper Kwaśny <kwasny.it@gmail.com>
2024-01-29 15:13:00 +0100

    README + mv cmd/volatile to main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"crypto/rand"
+	"math/big"
 	"sync"
 	"time"
 
 	n "github.com/kacpekwasny/noundo/pkg/noundo"
+	lor "gopkg.in/loremipsum.v1"
 
 	"github.com/kacpekwasny/noundo/pkg/noundo"
 	"github.com/kacpekwasny/noundo/pkg/utils"
@@ -15,6 +18,28 @@ func runUniverse(wg *sync.WaitGroup, uni *noundo.Universe, httpAddr string, grpc
 	n.SetupListen(httpAddr, grpcAddr)
 	utils.Loge(n.Serve())
 	wg.Done()
+}
+
+var uni0 *n.Universe
+var uni1 *n.Universe
+var uni2 *n.Universe
+var lorGen = lor.New()
+
+func createStories(h n.HistoryFullIface, m int, age string, author n.UserPublicIface, text string) {
+	for i := 0; i < m; i++ {
+		s, _ := h.CreateStory(author, age, &n.StoryContent{
+			Title:   lorGen.Words(5),
+			Content: lorGen.Sentences(rint(30)),
+		})
+		a1, _ := h.CreateAnswer(author, s.Id(), lorGen.Words(5+rint(30)))
+		a12, _ := h.CreateAnswer(author, a1.PostableId, lorGen.Words(5+rint(30)))
+		h.CreateAnswer(author, a12.PostableId, lorGen.Words(5+rint(30)))
+		h.CreateAnswer(author, s.Id(), lorGen.Words(5+rint(30)))
+	}
+}
+
+func rint(n int64) int {
+	return int(utils.LeftLogRight(rand.Int(rand.Reader, big.NewInt(n))).Int64())
 }
 
 func main() {
